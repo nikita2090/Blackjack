9ed3b91240074a5551c84372c211d88e14a940c5
Nikita <slevin070@gmail.com>
2019-08-18 16:52:29 +0300

    Main logic of application has moved from App component to GameTable.
    New component Header has added.
    App component now consist of Header and GameTable component.

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/*import PropTypes from ''*/
+
+/*import styles from './BetControls.module.css';*/
+
+
+const Header = () => (
+	<header>
+		<h1>Blackjack</h1>
+	</header>
+);
+
+export default Header;
