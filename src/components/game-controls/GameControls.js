acac49d93f7391a603ebfb463a6ba3f481f4be11
Nikita <slevin070@gmail.com>
2019-08-28 12:34:11 +0300

    Location of buttons in GameControls component has changed for better UX. Margin of GameControls wrap has changed from 14px to 5px.

--- src/components/game-controls/GameControls.js
+++ src/components/game-controls/GameControls.js
@@ -10,22 +10,23 @@ const { wrap, depositSt, panel } = styles;
 const GameControls = ({ winner, deposit, start, hit, stop, }) => (
 	<div className={wrap}>
 		<div className={panel}>
-			<ControlButton name='stop'
-			               value='STOP'
-			               disabled={!!winner}
-			               onClick={stop}/>
-
-			<div className={depositSt}>Your deposit: {deposit}</div>
-
 			<ControlButton name='start'
 			               value='START'
 			               disabled={!winner}
 			               onClick={start}/>
 
+
+			<div className={depositSt}>Your deposit: {deposit}</div>
+
 			<ControlButton name='hit'
 			               value='HIT'
 			               disabled={!!winner}
 			               onClick={hit}/>
+
+			<ControlButton name='stop'
+			               value='STOP'
+			               disabled={!!winner}
+			               onClick={stop}/>
 		</div>
 	</div>
 );
